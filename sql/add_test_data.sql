bbee70f954bf23a6dc64eabc3d483e8a70defb08
yanis <yanis@localhost.localdomain>
2017-09-15 21:21:19 +0200

    Päivitetty kannanluontilauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja(kayttajatunnus, salasana, rooli, tila) VALUES ('testi1'. 'salasana1'. 'asiakas', 'aktiivi');
+INSERT INTO Asiakas(henkilotunnus, sukunimi, etunimi, katuosoite, postinumero, postitoimipaikka, puhelinnumero, tila) VALUES ('010199-010A', 'Meikäläinen', 'Matti', 'Testikatu 1', '00000', 'HELSINKI', '050-0101010', 'aktiivi');
+INSERT INTO Sopimus(sopimustunnus, tila, turvanSuuruus, vakuutusmaksu) VALUES ('9999', 'aktiivi', '1000.00', '100.00');
+INSERT INTO Tuote(tuotetunnus, tuotenimi) VALUES ('100', 'Pariturva');
